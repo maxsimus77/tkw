c69b04f26754936068c0e5a0af4d546fb908dc75
maximus77 <ngonguyenducmanh@gmail.com>
2024-04-02 15:01:35 +0700

    update des

--- src/app/course/[id]/page.tsx
+++ src/app/course/[id]/page.tsx
@@ -8,33 +8,55 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import Image from "next/image"
 import { works, tutorial, blockchain } from "@/app/_asset/course_asset"
 
 export default function Page(props: any) {
     let id: number = props.params.id as number
-    
-    console.log(works)
+    const course = [];
+    for(let i = 0; i < works.length; i++) {
+        if(works[i].id == id) {
+            course.push(works[i])
+        }
+    }
+    for(let i = 0; i < tutorial.length; i++) {
+        if(tutorial[i].id == id) {
+            course.push(tutorial[i])
+        }
+    }
+    for(let i = 0; i < blockchain.length; i++) {
+        if(blockchain[i].id == id) {
+            course.push(blockchain[i])
+        }
+    }
 
     return (
         <div style={{ display: "flex", justifyContent: "space-between", flexDirection: "row", padding: "1rem" }}>
-            div
-            <h1>{props.params.id}</h1>
+            <div className="w-[25%] flex flex-col space-between mt-10 p-5">
+                <Image
+                    src={course[0].banner}
+                    alt={`Photo by ${course[0].name}`}
+                    className="h-fit w-fit object-cover"
+                    width={250}
+                    height={150}
+                />
+                <p>
+                    {course[0].description}
+                </p>
+            </div>
             <div className="w-[25%] flex flex-col space-between mt-10 p-5">
                 <Link href="/course" className="w-full mb-5"><Button>Back</Button></Link>
                 <Card>
                     <CardHeader>
                         <CardTitle>
-                            {}
+                            {course[0].name}
                         </CardTitle>
                         <CardDescription>
-                            Khóa học lập trình web
+                            {course[0].price === 0 ? "Miễn phí" : course[0].price + " USDT"}
                         </CardDescription>
                     </CardHeader>
-                    <CardContent>
-                        Khóa học lập trình web
-                    </CardContent>
                     <CardFooter>
-                        <Button variant="outline">Xem khóa học</Button>
+                        <Button variant="outline">{course[0].price === 0 ? "Đăng ký" : "Mua ngay"}</Button>
                     </CardFooter>
                 </Card>
             </div>
